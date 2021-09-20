4b899bc9a6dc73546a849a99ab23ba08b1cad5e1
erikaosterbur <erikaosterbur@gmail.com>
2021-09-20 13:31:43 -0500

    Updated math for ties, wins, and losses

--- script.js
+++ script.js
@@ -22,43 +22,43 @@ function runGame() {
     }
     console.log(oneTime);
     if (userSelection === rock && oneTime === rock) {
-        tie = tie + 1;
+        tie++;
         alert ("Computer chose Rock. Tie!");
     }
     else if (userSelection === rock && oneTime === paper) {
-        lose = lose + 1;
+        lose++;
          alert ("Computer chose Paper. Computer wins!");
         }
     else if (userSelection === rock && oneTime === scissors) {
-        win = win + 1;
+        win++;
          alert ("Computer chose Scissors. You win!");
     }
     else if (userSelection === paper && oneTime === paper) {
-        tie = tie + 1;
+        tie++;
          alert ("Computer chose Paper. Tie!");
     }
     else if (userSelection === paper && oneTime === rock) {
-        win = win + 1;
+        win++;
          alert ("Computer chose Rock. You win!");
     }
     else if (userSelection === paper && oneTime === scissors) {
          alert ("Computer chose Scissors. Computer wins!");
-         lose = lose + 1;
+         lose++;
     }
     else if (userSelection === scissors && oneTime === scissors) {
          alert ("Computer chose Scissors. Tie!");
-         tie = tie + 1;
+         tie++;
     }
     else if (userSelection === scissors && oneTime === rock) {
-        lose = lose + 1;
+        lose++;
          alert ("Computer chose Rock. Computer wins!");
     }
     else if (userSelection === scissors && oneTime === paper) {
          alert ("Computer chose Paper. You win!");
-         win = win + 1;
+         win++;
     }
     else {
-        window.confirm("Choose R, P, or S")
+        window.confirm("Choose R, P, or S") 
     }
     again = window.confirm("Wins = " + win + "\nLosses = " + lose + "\nTies = " + tie + "\nDo you want to play again?");
 }
